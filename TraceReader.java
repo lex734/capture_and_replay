a01b426e8895a7c9b6d93305b8b66111e0b5cb73
lex734 <lex@DESKTOP-LU4VMOS.localdomain>
2026-01-06 13:45:42 +0000

    trace reader updated for siteId

--- TraceReader.java
+++ TraceReader.java
@@ -3,13 +3,13 @@ import java.io.RandomAccessFile;
 public class TraceReader {
   public static void main(String[] args) throws Exception {
     // Must match the schema size
-    final int RECORD_SIZE = 24;
+    final int RECORD_SIZE = 28;
         
     try (RandomAccessFile file = new RandomAccessFile("trace.bin", "r")) {
       long fileSize = file.length();
       System.out.println("Reading trace file (" + fileSize + " bytes)...");
       System.out.println("---------------------------------------------------------------");
-      System.out.println(String.format("%-10s | %-10s | %-15s | %s", "SEQ", "THREAD_ID", "EVENT", "OBJ_HASH"));
+      System.out.println(String.format("%-10s | %-10s | %-15s | %-5s | %s", "SEQ", "THREAD_ID", "EVENT", "OBJ_HASH", "SITE_ID"));
       System.out.println("---------------------------------------------------------------");
 
       // Loop through the file 24 bytes at a time
@@ -18,11 +18,12 @@ public class TraceReader {
         long tid = file.readLong();
         int type = file.readInt();
         int hash = file.readInt();
+        int siteId = file.readInt();
 
         // Stop reading if we hit empty records (zeros)
         if (seq == 0 && tid == 0 && type == 0) break;
         String eventName = decodeEvent(type);
-        System.out.println(String.format("%-10d | %-10d | %-15s | %d", seq, tid, eventName, hash));
+        System.out.println(String.format("%-10d | %-10d | %-15s | %-5s | %d", seq, tid, eventName, hash, siteId));
       }
     }
   }

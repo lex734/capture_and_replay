650985aa56b996b8f8826968cfdb9ea4f5924024
lex734 <enxinglao@gmail.com>
2026-01-24 19:11:20 +0000

    update capture agent

--- capture/CaptureMonitor.java
+++ capture/CaptureMonitor.java
@@ -1,29 +1,37 @@
 package capture;
 
-import common.BinarySchema;
-import common.IdentityMapper;
-import java.util.concurrent.atomic.AtomicLong;
+import common.TraceLogger;
 
 public class CaptureMonitor {
-  private static final AtomicLong clock = new AtomicLong(0);
   private static final ThreadLocal<Boolean> isInside = ThreadLocal.withInitial(() -> false);
 
   public static void logSync(Object lock, int eventType, int siteId) {
     if (isInside.get() || lock == null) return;
     isInside.set(true);
     try {
-        long seq = clock.getAndIncrement();
-        
-        // Identity is derived from the CODE, not the MEMORY
-        int logicalObjectId = IdentityMapper.getLogicalObjectId(siteId);
-        int roleId = IdentityMapper.getRoleIdBySite(Thread.currentThread().getId(), siteId);
-
-        System.out.println("[Capture] Seq: " + seq + " | Site: " + siteId + " | Role: " + roleId + " | Type: " + eventType);
-        // We still store the eventType (Enter/Exit) and the siteId
-        BinarySchema.write(seq, (long)roleId, eventType, logicalObjectId, siteId);
-        
+      TraceLogger.logSync(eventType, lock, siteId);        
     } finally {
         isInside.set(false);
+    }
+  }
+
+  public static void logField(int eventType, Object owner, int fieldId, int currentSiteId, boolean isVolatile, boolean isStatic) {
+    if (isInside.get()) return;
+    isInside.set(true);
+    try {
+      TraceLogger.logField(eventType, owner, currentSiteId, isVolatile, isStatic, fieldId);
+    } finally {
+      isInside.set(false);
+    }
+  }
+  
+  public static void logArray(int eventType, Object array, int index, int currentSiteId) {
+    if (isInside.get() || array == null) return;
+    isInside.set(true);
+    try {
+      TraceLogger.logArray(eventType, array, index, currentSiteId);
+    } finally {
+      isInside.set(false);
     }
   }
 }
--- test_app/Main.java
+++ test_app/Main.java
@@ -1,11 +1,23 @@
 public class Main {
     private static final Object lock = new Object();
 
+    private static int count = 0;
+
     public static void main(String[] args) throws InterruptedException {
         System.out.println("[App] Starting threads...");
 
-        Thread t1 = new Thread(() -> work("Worker-1"));
-        Thread t2 = new Thread(() -> work("Worker-2"));
+        int a = 1;
+
+        Thread t1 = new Thread(() ->{
+            for (int i = 0; i < 50; i++) {
+                count++;
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 50; i++) {
+                count++;
+            }
+        });
 
         t1.start();
         t2.start();
@@ -16,14 +28,14 @@ public class Main {
         System.out.println("[App] Done.");
     }
 
-    private static void work(String name) {
-        System.out.println(name + " attempting to lock...");
-        synchronized (lock) {
-            System.out.println(name + " acquired lock!");
-            try {
-                Thread.sleep(100); // Hold it for a bit
-            } catch (InterruptedException e) { }
-            System.out.println(name + " releasing lock.");
-        }
-    }
+    // private static void work(String name) {
+    //     System.out.println(name + " attempting to lock...");
+    //     synchronized (lock) {
+    //         System.out.println(name + " acquired lock!");
+    //         try {
+    //             Thread.sleep(100); // Hold it for a bit
+    //         } catch (InterruptedException e) { }
+    //         System.out.println(name + " releasing lock.");
+    //     }
+    // }
 }
